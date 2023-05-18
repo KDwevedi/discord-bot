9e3272b5339154f8e6f7178c9fd5a1df1f3a536c
KDwevedi <kanav11dwevedi@gmail.com>
2023-05-18 20:44:12 +0530

    Initialising Test Bot

---
+++ test-bot.py
@@ -0,0 +1,21 @@
+import discord
+
+#What are intents?
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('MTEwODc1OTUzMTg5NjQ0MzAxMQ.GXuV8K.o-MsG-jDbDXDOSMCOpG8oZbyNOkrsaEmaj3cyI')
